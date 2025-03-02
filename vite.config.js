07d5693d078bee89e8af7eb57c86672afee87119
unknown <skyarff@gmail.com>
2025-03-02 23:48:20 +0400

    запуск билда локально, преодоление заголовков

--- vite.config.js
+++ vite.config.js
@@ -23,24 +23,11 @@ export default defineConfig({
   define: { 'process.env': {} },
   assetsInclude: ['**/*.wasm'],
   build: {
-    assetsInlineLimit: 0, // Предотвратит встраивание WASM в JS
+    assetsInlineLimit: 0,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
 })
